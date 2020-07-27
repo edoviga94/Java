5b467882b40ee7eff42292568ed4a0e1e4e5d8c9
Edoardo Viganò <e.vigano24@campus.unimib.it>
2020-07-27 11:41:03 +0200

    Esercizio4

---
+++ Esercitazione3.1/Esercizio4/src/TestEsercizio4.java
@@ -0,0 +1,53 @@
+import java.util.LinkedList;
+
+public class TestEsercizio4 {
+
+	public static LinkedList<LinkedList<Integer>> insiemeDiInsiemi(int n) {
+		LinkedList<LinkedList<Integer>> list = new LinkedList<LinkedList<Integer>>();
+
+		for (int i = 0; i < n; i++) {
+
+			LinkedList<Integer> intern = new LinkedList<Integer>();
+			for (int j = 0; j <= i; j++) {
+				intern.add(j);
+			}
+
+			list.add(intern);
+		}
+
+		return list;
+	}
+
+	
+	public static void stampa(LinkedList<LinkedList<Integer>> a) {
+		
+		
+		for (int i = 0; i < a.size(); i++) {
+			LinkedList<Integer> intern = a.get(i);
+			System.out.print("Elemento "+(i+1)+": ");
+			for (int j = 0; j < intern.size() ; j++) {
+				
+				System.out.print("<" + intern.get(j) + "> ");
+				
+			}
+			System.out.println();
+		}
+		
+	}
+	
+	
+	public static void testEs4() {
+		int dim = (int) (Math.random() * 20) + 1;
+		System.out.println("Dimensione della LinkedList: "+dim+"\n");
+
+		
+		LinkedList<LinkedList<Integer>> lista = insiemeDiInsiemi(dim);
+		
+		stampa(lista);
+	}
+
+	public static void main(String[] args) {
+		testEs4();
+	}
+
+}
